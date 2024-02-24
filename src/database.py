7dcee2310c3134b974315e7ea44b587be208696c
Mason Hicks <mhicks071@yahoo.com>
2024-02-24 16:05:20 -0500

    Added code to connect to database

---
+++ src/database.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{os.environ['POSTGRES_USER']}" +
+    f":{os.environ['POSTGRES_PASSWORD']}" +
+    f"@{os.environ['POSTGRES_ADDRESS']}:5432" +
+    f"/{os.environ['POSTGRES_DB']}"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base
